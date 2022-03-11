fc75f9a50e63cea791bb3f0a5769cd4b3ab3affc
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-03-11 21:26:51 +0900

    Modify : Beverage Models

--- Beverage/models.py
+++ Beverage/models.py
@@ -1,15 +1,12 @@
-from multiprocessing.spawn import old_main_modules
-from tkinter import CASCADE
-from unicodedata import category
 from django.db import models
-from django.forms import CharField
+
 
 # Create your models here.
 class Menu(models.Model):
     name = models.CharField(max_length=45)
 
     class Meta:
-        db_table = 'menu'
+        db_table = 'product_menu'
 
 
 class Category(models.Model):
@@ -17,7 +14,7 @@ class Category(models.Model):
     name = models.CharField(max_length=45)
 
     class Meta:
-        db_table = 'categories'
+        db_table = 'product_categories'
 
 
 class Drink(models.Model):
@@ -25,13 +22,14 @@ class Drink(models.Model):
     korean_name = models.CharField(max_length=45)
     english_name = models.CharField(max_length=45)
     description = models.TextField()
-    arrergy = models.ManyToManyField('Allergy')
+    allergy = models.ManyToManyField('Allergy','self')
 
     class Meta:
-        db_table = 'drink'
+        db_table = 'product_drink'
 
 class Nutritions(models.Model):
     drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    size_id = models.ForeignKey('Sizes', on_delete=models.CASCADE)
     one_serving_kcal= models.DecimalField(decimal_places=1,max_digits=2)
     sodium_mg = models.DecimalField(decimal_places=2,max_digits=2)
     saturated_fat_g = models.DecimalField(decimal_places=2,max_digits=2)
@@ -40,18 +38,28 @@ class Nutritions(models.Model):
     caffeine_mg = models.DecimalField(decimal_places=2,max_digits=2)
 
     class Meta:
-        db_table = 'nutritions'
+        db_table = 'product_nutritions'
         
 
 class Allergy(models.Model):
     name = models.CharField(max_length=45)
 
     class Meta:
-        db_table = 'allergy'
+        db_table = 'product_allergy'
 
 class Images(models.Model):
     img_url = models.CharField(max_length=2000)
     drink = models.ForeignKey('Drink', on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'images'
+        db_table = 'product_images'
+
+
+class Sizes(models.Model):
+    name = models.CharField(max_length=45)
+    size_ml = models.CharField(max_length=45, null=True)
+    size_fluid_ounce = models.CharField(max_length=45, null=True)
+
+    class Meta:
+        db_table = 'product_size'
+        
