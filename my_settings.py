f9705736a39ed9a44f69979d32b82cfcd9783118
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-03-11 14:52:53 +0900

    ADD : WeStarbucks DIR& Setting_origin

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'django-insecure-t)wn+htq)jwsmw5eoi1do@o9&%b4(4=o$q#nq0l1t7nco2n()v'
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER' :'root' , 
+        'PASSWORD' : 'ghkdwjdgus12!',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+    }
+}
